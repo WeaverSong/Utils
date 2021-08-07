240708f97722563b7f8c154382ac0b564641beec
WeaverSong <Legozipper@gmail.com>
2021-08-06 23:35:58 -0600

    Added VecX class Updated Vec2 description

--- Vectors.js
+++ Vectors.js
@@ -1,4 +1,4 @@
-//Takes an object with x and y, or 2 parameters.
+//Takes an object with x and y, an array of 2 items, 2 parameters, or 1 parameter.
 class Vec2 {
     constructor (x, y)
     {
@@ -111,3 +111,115 @@ class Vec2 {
         return vec.Divide(vec.Magnitude());
     };
 };
+
+//Creates a vector with an arbitrary number of values. Can take in a VecX, Vec2, Vec3, Array, or number;
+class VecX {
+    constructor (values) {
+        if (typeof(values) === "number") values = [values];
+        else if (typeof(values) === "object" && typeof(values.values) === "object") values = values.values;
+        else if (typeof(values) === "object" && values.x !== undefined && values.z === undefined) values = [values.x, values.y];
+        else if (typeof(values) === "object" && values.x !== undefined) values = [values.x, values.y, values.z];
+        this.values = values;
+        this.size = values.length;
+    };
+
+    //Adds a second Vector-type. Returns this.
+    Add (values) {
+        let vec = new VecX (values);
+        while (vec.size < this.size) {
+            vec.values.push(vec.values[0]);
+            vec.size++;
+        };
+
+        this.values = this.values.map((val, index) => val + vec.values[index]);
+
+        return this;
+    };
+    //Subtracts a second Vector-type. Returns this.
+    Subtract (values) {
+        let vec = new VecX (values);
+        while (vec.size < this.size) {
+            vec.values.push(vec.values[0]);
+            vec.size++;
+        };
+
+        this.values = this.values.map((val, index) => val - vec.values[index]);
+
+        return this;
+    };
+    //Multiplies by a second Vector-type. Returns this.
+    Multiply (values) {
+        let vec = new VecX (values);
+        while (vec.size < this.size) {
+            vec.values.push(vec.values[0]);
+            vec.size++;
+        };
+
+        this.values = this.values.map((val, index) => val * vec.values[index]);
+
+        return this;
+    };
+    //Divides by a second Vector-type. Returns this.
+    Divide (values) {
+        let vec = new VecX (values);
+        while (vec.size < this.size) {
+            vec.values.push(vec.values[0]);
+            vec.size++;
+        };
+
+        this.values = this.values.map((val, index) => val / vec.values[index]);
+
+        return this;
+    };
+    //Normalizes this VecX. Returns this.
+    Normalize () {
+        this.values = VecX.Normalized(this).values;
+        return this;
+    };
+    //Returns the magnitude of this VecX
+    Magnitude () {
+        return VecX.Magnitude(this);
+    };
+    //Returns the normalized equivalent of this VecX
+    Normalized () {
+        return VecX.Normalized(this);
+    }
+
+
+    //Static methods
+
+    //Adds 2 Vector-types
+    static Add (vec1, vec2) {
+        vec1 = new VecX (vec1);
+        vec2 = new VecX (vec2);
+        return vec1.Add(vec2);
+    };
+    //Subtracts 2 Vector-types
+    static Subtract (vec1, vec2) {
+        vec1 = new VecX (vec1);
+        vec2 = new VecX (vec2);
+        return vec1.Subtract(vec2);
+    };
+    //Multiplies 2 Vector-types
+    static Add (vec1, vec2) {
+        vec1 = new VecX (vec1);
+        vec2 = new VecX (vec2);
+        return vec1.Multiply(vec2);
+    };
+    //Adds 2 Vector-types
+    static Divide (vec1, vec2) {
+        vec1 = new VecX (vec1);
+        vec2 = new VecX (vec2);
+        return vec1.Divide(vec2);
+    };
+    //Returns the magnitude of a Vector-type
+    static Magnitude (vec) {
+        vec = new VecX (vec);
+        return Math.sqrt(vec.values.reduce((adder, value) => adder + value * value, 0));
+    };
+    //Returns the normalized form of a Vector-type
+    static Normalized (vec) {
+        vec = new VecX (vec);
+        return vec.Divide(vec.Magnitude());
+    };
+}
