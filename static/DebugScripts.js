05b51e899eb21540cd6432b5d836b76e0a5bc561
WeaverSong <Legozipper@gmail.com>
2021-08-20 18:59:18 -0600

    fix

--- static/DebugScripts.js
+++ static/DebugScripts.js
@@ -4,24 +4,4 @@ let CR = new CanvasRenderer();
 let MM = new MapManager();
 document.body.appendChild(CR.canvas);
 
-let h = html;
-
-let Res = h.wrap(
-    h.style({
-        body: {
-            display: "flex"
-        },
-
-        div: {
-            "background-color": "black",
-            width: "100px",
-            height: "100px",
-            display: "inline-block"
-        }
-    }),
-    
-    h.div(),
-    h.div({id: "hi"})
-);
-
-//console.log(Res);
+let h = NearlyJSX;
