fc96a06d883340068d8565548a66585e5286d1f0
WeaverSong <Legozipper@gmail.com>
2021-08-23 21:56:52 -0600

    Finished adding basic methods

--- static/Url.js
+++ static/Url.js
@@ -1,12 +1,68 @@
 const url = {
     post: {
-        json: async function (url, body, headers = {}) {
-            let PostHeaders = {"content-type": "application/json"};
+        json: async function (url, body = {}, headers = {})
+        {
+            let PostHeaders = { "content-type": "application/json" };
             Object.assign(PostHeaders, headers);
 
             let data = await fetch(url, {
-               method: "POST",
-               headers: PostHeaders,
+                method: "POST",
+                headers: PostHeaders,
+                body: JSON.stringify(body)
+            });
+
+            let ResBody = await data.json();
+            data.body = ResBody;
+
+            //Not rejecting, so you still get the data returned
+            return data;
+        }
+    },
+    put: {
+        json: async function (url, body = {}, headers = {})
+        {
+            let PutHeaders = { "content-type": "application/json" };
+            Object.assign(PutHeaders, headers);
+
+            let data = await fetch(url, {
+                method: "PUT",
+                headers: PutHeaders,
+                body: JSON.stringify(body)
+            });
+
+            let ResBody = await data.json();
+            data.body = ResBody;
+
+            //Not rejecting, so you still get the data returned
+            return data;
+        }
+    },
+    get: {
+        json: async function (url, headers = {})
+        {
+            let GetHeaders = {};
+            Object.assign(GetHeaders, headers);
+
+            let data = await fetch(url, {
+                method: "GET",
+                headers: GetHeaders
+            });
+
+            let ResBody = await data.json();
+            data.body = ResBody;
+
+            //Not rejecting, so you still get the data returned
+            return data;
+        }
+    },
+    delete: {
+        json: async function (url, body = {}, headers = {}) {
+            let DeleteHeaders = {"content-type": "application/json"};
+            Object.assign(DeleteHeaders, headers);
+
+            let data = await fetch(url, {
+               method: "DELETE",
+               headers: DeleteHeaders,
                body: JSON.stringify(body)
             });
 
